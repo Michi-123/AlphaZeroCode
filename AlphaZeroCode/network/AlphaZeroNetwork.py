Fix SelfPlay terminal reward sign, ResNet double application, Agent get_legal_actions, Evaluate win counting

AlphaZeroCode/network/AlphaZeroNetwork.py
@@ -115,8 +115,7 @@ class AlphaZeroNetwork(nn.Module):
         x = F.relu(x, inplace=True)
 
         """ Residual blocks """
-        for i in range(self.CFG.n_residual_block):
-            x = self.resnet(x)
+        x = self.resnet(x)
 
         return x
 